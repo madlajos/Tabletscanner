ab66ed25460838c4bbffba639edb1405bf31abe2
Lali <madlajos96@gmail.com>
2026-01-06 15:29:52 +0100

    Set axis boundaries

--- Firmware/Marlin/Configuration.h
+++ Firmware/Marlin/Configuration.h
@@ -1911,15 +1911,15 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 172
+#define X_BED_SIZE 175
+#define Y_BED_SIZE 175
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 100
+#define Z_MAX_POS 25
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
